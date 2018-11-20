4ee2bbb9fabb5c1582b48f2ea6b7e1d72f6f72d2
vk.rakin <vk.rakin@tensor.ru>
2018-11-20 22:26:38 +0500

    Add calendar and icon

--- app/src/main/java/com/example/vlad/todolist/EditEvent.java
+++ app/src/main/java/com/example/vlad/todolist/EditEvent.java
@@ -1,13 +1,51 @@
 package com.example.vlad.todolist;
 
+import android.app.DatePickerDialog;
+
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class EditEvent extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class EditEvent extends AppCompatActivity implements  DatePickerDialog.OnDateSetListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_event);
+
+        ImageButton btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        String currentDate = DateFormat.getDateInstance().format(c.getTime());
+        TextView tv = findViewById(R.id.textView);
+        tv.setText(currentDate);
     }
 }
